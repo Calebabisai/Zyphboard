deb0cd553503602581521bad3abbb7f6e2eb8b89
Calebabisai <gelndcaleb@gmail.com>
2025-11-30 21:42:53 -0600

    Cambio: Todos los componentes se ha modificado a Standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        loadComponent: () => import('./layouts/main-layout/main-layout').then(m => m.MainLayout),
+        children: [
+        {
+            path: 'dashboard',
+            loadComponent: () => import('./features/dashboard/dashboard').then(m => m.Dashboard)
+        },
+        {
+            path: 'users',
+            loadComponent: () => import('./features/users/users').then(m => m.Users)
+        },
+        {
+            path: 'analytics',
+            loadComponent: () => import('./features/analytics/analytics').then(m => m.Analytics)
+        },
+        {
+            path: 'settings',
+            loadComponent: () => import('./features/settings/settings').then(m => m.Settings)
+        }
+        ]
+    },
+    {
+        path: 'auth',
+        loadComponent: () => import('./layouts/auth-layout/auth-layout').then(m => m.AuthLayout),
+        children: [{
+            path: 'login',
+            loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
+        }]
+    }
+];
---
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.css',
+})
+export class AuthLayout {
+
+}
