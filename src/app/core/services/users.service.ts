9b531ccefeafa7eb1693bc46337ecd5fbbda5bd5
Calebabisai <gelndcaleb@gmail.com>
2025-12-04 23:30:15 -0600

    cambio: Creacion de funciones para generar usuarios por id temporalmente, lista de usuarios y obtener usuarios por su id, al igual que uso computed como signal reactivo, para usar en el DOM

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -2,16 +2,35 @@ import { Injectable, computed } from '@angular/core';
 import { persistentSignal } from '../../shared/utils/persistent-signal';
 import { WritableSignal } from '@angular/core';
 import { User } from '../../models/user.model';
+// Definición de la clave de almacenamiento
+const STORAGE_KEY = 'zyphboard:users';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
+  /** Señal que almacena la lista de usuarios */
   private readonly _users: WritableSignal<User[]>; 
 
   activeUsers = computed(() => this._users().filter(user => user.active));
+  userCount = computed(() => this._users().length);
 
   constructor() {
-    this._users = persistentSignal<User[]>('zyphboard:users', []);
+    this._users = persistentSignal<User[]>(STORAGE_KEY, []);
   }
+
+  private generateId(): string {
+    // id simple, para seguir desarrollando de forma locarl y temporal
+    return Date.now().toString(36) + Math.random().toString(36).slice(2, 8);
+  }
+
+  list() : User[] {
+    return this._users();
+  }
+
+  get(id: string) : User | undefined {
+    return this._users().find(user => user.id === id);
+  }
+
+
 }
